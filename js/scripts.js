be0279d805c17e19a6bf037d1dac22d194bc641e
karolinneczka <okaa93@gmail.com>
2018-07-06 23:15:59 +0200

    Works for more countries

--- js/scripts.js
+++ js/scripts.js
@@ -29,19 +29,19 @@ function showCountries(resp) {
 }
 
 function createHeader(item) {
-	$('<div>').attr('class', 'header').appendTo('#countries');
-	$('<div>').attr('class', 'flag').appendTo('.header');
-	$('<div>').attr('class', 'name').appendTo('.header');
-	$('<img>').attr("src",flagUrlPrefix+item.alpha2Code+flagUrlPostfix).appendTo('.flag');
-	$('.name').append(item.name);
+	var header = $('<div>').attr('class', 'header').appendTo('#countries');
+	var flag = $('<div>').attr('class', 'flag').appendTo(header);
+	var name = $('<div>').attr('class', 'name').appendTo(header);
+	$('<img>').attr("src",flagUrlPrefix+item.alpha2Code+flagUrlPostfix).appendTo(flag);
+	$(name).append(item.name);
 }
 
 function createList(item) {
-	$('<div>').attr('class', 'countryList').appendTo('#countries');
-	$('<div>').attr('class', 'row-1').appendTo('.countryList');
-	$('<span>').attr('class', 'detail').text('Country code:').appendTo('.row-1');
-	$('<span>').attr('class', 'value').text(item.alpha2Code).appendTo('.row-1');
-	$('<div>').attr('class', 'row-2').appendTo('.countryList');
-	$('<span>').attr('class', 'detail').text('Population:').appendTo('.row-2');
-	$('<span>').attr('class', 'value').text(item.population).appendTo('.row-2');
+	var countryList = $('<div>').attr('class', 'countryList').appendTo('#countries');
+	var row1 = $('<div>').attr('class', 'row-1').appendTo(countryList);
+	$('<span>').attr('class', 'detail').text('Country code:').appendTo(row1);
+	$('<span>').attr('class', 'value').text(item.alpha2Code).appendTo(row1);
+	var row2 = $('<div>').attr('class', 'row-2').appendTo(countryList);
+	$('<span>').attr('class', 'detail').text('Population:').appendTo(row2);
+	$('<span>').attr('class', 'value').text(item.population).appendTo(row2);
 }
