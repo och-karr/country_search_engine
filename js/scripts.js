a030d245b6ee097bccfe3b5971908373ee6bffa5
karolinneczka <okaa93@gmail.com>
2018-10-24 12:40:25 +0200

    Finish the task, add interesting look

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,80 @@
 var url = 'https://restcountries.eu/rest/v2/name/';
-var flagUrlPrefix = 'http://www.countryflags.io/';
-var flagUrlPostfix = '/shiny/64.png';
+var flagUrlPrefix = 'https://restcountries.eu/data/';
+var flagUrlPostfix = '.svg';
 
-var countryInput = $('#country-name');
-// var countryList = $('#countryList');
+var $countryInput = $('#country-name');
+var $countries = $('#countries');
 
 $('#search').on('click', searchCountries);
 
 function searchCountries(){
-	var countryName = countryInput.val();
+	var countryName = $countryInput.val();
 	if(!countryName.length) countryName = 'Poland';
 
 	$.ajax({
 		url: url + countryName,
 		method: 'GET',
-		success: showCountries
+		success: countryCard
 	});
 }
 
-function showCountries(resp) {
-	$('#countries').empty(); 
+function countryCard(resp) {
+	$countries.empty(); 
 	if(resp.length) { //jesli resp ma dlugosc (tzn zawartosc)
+
 		resp.forEach(function(item) {
+			var $country = $('<div>').addClass('country');
+			$countries.append($country);
 			createHeader(item);
 			createList(item);
+
+			function createHeader(item) {
+				var code = item.alpha3Code.toLowerCase();
+				var $header = $('<div>').addClass('header');
+				var $flag = $('<span>').addClass('flag');
+				var $name = $('<span>').addClass('name').text(item.name);
+				var $countryImage = $('<img>').attr("src",flagUrlPrefix+code+flagUrlPostfix);
+			
+				$country.append($header);
+				$header.append($flag)
+						.append($name);
+				$flag.append($countryImage);
+	
+				if(item.name.length>12) {
+					$name.css("font-size", "14px");
+					$name.css("padding-top", "0");
+				}
+			}
+	
+			function createList(item) {
+				var $countryList = $('<div>').addClass('countryList');
+				var $row1 = $('<tr>').addClass('row-1');
+				var $detail1 = $('<td>').addClass('detail').text('Country code:');
+				var $value1 = $('<td>').addClass('value').text(item.alpha2Code);
+				var $row2 = $('<tr>').addClass('row-2');
+				var $detail2 = $('<td>').addClass('detail').text('Capital:');
+				var $value2 = $('<td>').addClass('value').text(item.capital);
+				var $row3 = $('<tr>').addClass('row-2');
+				var $detail3 = $('<td>').addClass('detail').text('Population:');
+				var $value3 = $('<td>').addClass('value').text(item.population);
+				var $row4 = $('<tr>').addClass('row-2');
+				var $detail4 = $('<td>').addClass('detail').text('Currency:');
+				var $value4 = $('<td>').addClass('value').text(item.currencies[0].name);
+			
+				$country.append($countryList);
+				$countryList.append($row1)
+							.append($row2)
+							.append($row3)
+							.append($row4);
+				$row1.append($detail1)
+						.append($value1);
+				$row2.append($detail2)
+						.append($value2);
+				$row3.append($detail3)
+						.append($value3);
+				$row4.append($detail4)
+						.append($value4);
+			}
 		});
 	}
-}
-
-function createHeader(item) {
-	var header = $('<div>').attr('class', 'header').appendTo('#countries');
-	var flag = $('<div>').attr('class', 'flag').appendTo(header);
-	var name = $('<div>').attr('class', 'name').appendTo(header);
-	$('<img>').attr("src",flagUrlPrefix+item.alpha2Code+flagUrlPostfix).appendTo(flag);
-	$(name).append(item.name);
-}
-
-function createList(item) {
-	var countryList = $('<div>').attr('class', 'countryList').appendTo('#countries');
-	var row1 = $('<div>').attr('class', 'row-1').appendTo(countryList);
-	$('<span>').attr('class', 'detail').text('Country code:').appendTo(row1);
-	$('<span>').attr('class', 'value').text(item.alpha2Code).appendTo(row1);
-	var row2 = $('<div>').attr('class', 'row-2').appendTo(countryList);
-	$('<span>').attr('class', 'detail').text('Population:').appendTo(row2);
-	$('<span>').attr('class', 'value').text(item.population).appendTo(row2);
 }
